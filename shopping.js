9077afdd91b54de44e605f9a798446990ca68e65
daldan <daniel.aldan@innoveo.com>
2026-03-23 13:28:58 +0100

    Adding the shopping function

---
+++ shopping.js
@@ -0,0 +1,11 @@
+let inventory = [];
+
+const autoclicker1 = 0.1
+const autoclicker2 = 0.5
+const autoclicker3 = 1
+const autoclicker4 = 2
+const autoclicker5 = 4
+
+function buyItem(itemname) {
+    inventory.push(itemname)
+}
