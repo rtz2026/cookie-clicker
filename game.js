c167710017ed67fb82a12ede71f475178e7b412e
daldan <daniel.aldan@innoveo.com>
2026-03-23 13:41:21 +0100

    Adding the buy item option

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 import * as clicker from "clicker.js";
+import * as shopping from "shopping.js";
 
 let score = 0;
 
@@ -6,8 +7,17 @@ function addClicker(){
     document.getElementById("cookie").addEventListener(click, () => { score = clicker.incrementScore(score)})
 }
 
+function addItem(){
+    document.getElementById("Item1").addEventListener(click, () => {shopping.buyItem(autoclicker1)})
+    document.getElementById("Item2").addEventListener(click, () => {shopping.buyItem(autoclicker2)})
+    document.getElementById("Item3").addEventListener(click, () => {shopping.buyItem(autoclicker3)})
+    document.getElementById("Item4").addEventListener(click, () => {shopping.buyItem(autoclicker4)})
+    document.getElementById("Item5").addEventListener(click, () => {shopping.buyItem(autoclicker5)})
+}
+
 function gameSetup(){
     addClicker();
+    addItem();
 }
 
 gameSetup();
