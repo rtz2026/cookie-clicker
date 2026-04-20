01980c4e2a794c24e478c91ac98160f15f51d3c5
illeskeczan <illes.keczan@innoveo.com>
2026-04-20 14:04:11 +0200

    add AutoIncrement to App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useState } from "react";
 import './App.css'
+import './AutoClicker.jsx'
 
 function App() {
 
+  const [score, setScore] = useState(100);
+  const [inventory, setInventory] = useState([]);
+
+  const handleAutoTick = useCallback((amount) => {
+    setScore((prev) => prev + amount);
+  }, []);
+
+  const autoPerSecond = useMemo(() => {
+    return inventory.reduce((sum, item) => sum + item.autoPerSecond, 0);
+  }, [inventory]);
+
   return (
     <>
+    <div>
+      <AutoIncrementer rate={autoPerSecond} onTick={handleAutoTick} />
+    </div>
+    <div>
+      <h1>Score: {score.toFixed(2)}</h1>
+      <img src="/picture/cookie.png" alt="Cookie" />
+    </div>
+    <p>Auto/sec: {autoPerSecond.toFixed(2)}</p>
       
     </>
   )
